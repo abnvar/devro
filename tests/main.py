442ed1c42f0664d50c7cc3a6cd9223fe539da7a9
brunopinto900 <48806267+brunopinto900@users.noreply.github.com>
2020-04-22 15:57:20 +0100

    new main.py
    New main,py according to recent changes in components.py

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,8 @@
+import os, sys
+
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
 import cv2
 import simpy
 import numpy as np
@@ -8,8 +13,11 @@ from devro.slam.slam import GeneticSLAM
 
 envMap = getRandomMap(randbase = 1)
 
-lidar = Lidar(ppr = 360, range_ = 6, resolution = 0.05)
-bot = Bot(lidar=lidar, wheelDist=0.2)
+lidar = Lidar(ppr=360, range_=2, minDist = 0.1, resolution=0.05, 
+                 angleField = 4.01,
+                 distScannerToRobotCenter = 0.03)
+
+bot = Bot(lidar=lidar, wheelDist=0.2, wheelRadius=0.2)
 sim = Simulation(pixelSpan = 720, distSpan = 10, dt = 100, envMap = envMap, bot = bot, visualize = True)
 sim.begin()
 
